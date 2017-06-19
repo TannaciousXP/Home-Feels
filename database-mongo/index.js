3eb1ca95d9e61461ca59dfdf9e08fbde4e730f4a
Peter Tan <pxhtan@gmail.com>
2017-06-19 12:30:57 -0700

    conceptualize the proper schema and models

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -11,10 +11,31 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
+// create a schema for styles
+  // designSchema
+    // name {type: String, Unique: true}
+
 var itemSchema = mongoose.Schema({
   quantity: Number,
   description: String
 });
+
+// create a schemea for interiorDesignerSchemea
+  // name: {type: String, Unqiue: true}
+  // bio: String
+  // tips: String
+  // designtype: foreign key of designSchema name
+
+/*
+Charles and Ray Eames for mid-century modern                        
+Poul Henningsen for Scandinavian                        
+make sure you get the spelling right babe                        
+Joseph Dirand for French/Parisian
+ */
+
+// Create models for designSchema mid-century modern, scandinavian, french/parisian
+// create models for interiorsDesignerSchema Charles and Ray, Poul, Jospeh
+
 
 var Item = mongoose.model('Item', itemSchema);
 
